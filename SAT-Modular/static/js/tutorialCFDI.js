e113b22b9516394032b3bc2c8c8084d1b186f9b4
JESG-27 <jose.salcido4178@alumnos.udg.mx>
2023-06-29 15:56:21 -0600

    Configuración datos del emisor

--- SAT-Modular/static/js/tutorialCFDI.js
+++ SAT-Modular/static/js/tutorialCFDI.js
@@ -52,19 +52,48 @@ var Login1 = `
     <img class="captura" src="/static/images/CFDI/Login-pt2.png" alt="image">
     
     <div class="sesion">
-        <form action="#" method="post">
-            <input type="text" placeholder="RFC" required/>
-            <input type="password" placeholder="Contraseña" required/>
-            <input type="text" placeholder="Escribe la palabra del captcha" required/>
-            <button type="submit">Enviar</button>
+        <form>
+            <input type="text" placeholder="RFC" required onclick="actualizarTexto(6)" id="RFC" onkeyup="this.value = this.value.toUpperCase();">
+            <input type="password" placeholder="Contraseña" required onclick="actualizarTexto(7)" id="contrasena">
+            <input type="text" placeholder="Escribe la palabra del captcha" required onclick="actualizarTexto(8)" id="captcha">
+            <!-- <input type="button" onclick="val_login()" value="Enviar"> -->
+            <input type="button" onclick="siguiente(), actualizarTexto(10)" value="Enviar">
         </form>
-
     </div>    
 </div>
 <input type="button" onclick="anterior(), actualizarTexto(4)" value="Atras">
 `;
 
-//<input type="button" onclick="siguiente(), actualizarTexto()" value="EJECTUAR EN LINEA">
+// Posición 6
+var config_pt1 = `
+<div class="tuto-window">
+    <img class="captura" src="/static/images/CFDI/Config-1.png" alt="image">
+    <input type="button" onclick="siguiente(), actualizarTexto(11)" value="engrane">
+</div>
+<input type="button" onclick="anterior(), actualizarTexto(5)" value="Atras">
+`;
+
+// Posición 7
+var config_pt2 = `
+<div class="tuto-window">
+    <img class="captura" src="/static/images/CFDI/Config-2.png" alt="image">
+    <input type="button" onclick="siguiente(), actualizarTexto(12)" value="Configuración de datos del emisor">
+</div>
+<input type="button" onclick="anterior(), actualizarTexto(10)" value="Atras">
+`;
+
+// Posición 8
+var config_datos_emisor = `
+<div class="tuto-window">
+    <img class="captura" src="/static/images/CFDI/Datos-emisor-pt1.png" alt="image">
+    <img class="captura" src="/static/images/CFDI/Datos-emisor-pt2.png" alt="image">
+    <input type="text" onclick="actualizarTexto(6)" id="RFC" onkeyup="this.value = this.value.toUpperCase();">
+    <input type="text" onclick="actualizarTexto(13)" id="nombre">
+    <input type="text" onclick="actualizarTexto(14)">
+    <input type="button" onclick="siguiente(), actualizarTexto()" value="Guardar">
+</div>
+<input type="button" onclick="anterior(), actualizarTexto(11)" value="Atras">
+`;
 
 // Textos de guía
 // Posición 0
@@ -94,12 +123,12 @@ Puedes ver información referente a la facturación, para comenzar haz click en 
 
 // Posición 5
 var login = `
-Aquí se realiza el inicio de sesión, como se entra desde Mis cuentas no se requiere al e.firma
+Aquí se realiza el inicio de sesión, como se entraste desde "Mis cuentas" no se requiere al e.firma
 `;
 
 // Posición 6
 var RFC_login = `
-El RFC en una clave tu única alfanumérica compuesta de 13 letras y números, para este ejemplo ingresa: XXXXXXXXXXXXX
+El RFC en una clave tu única alfanumérica compuesta de 13 letras y números, para este ejemplo ingresa: ABCD123456XXX
 `;
 
 // Posición 7
@@ -109,25 +138,93 @@ Para este ejemplo la contraseña sera sat-sim, recuerda que tus contraseñas deb
 
 // Posición 8
 var captcha_login = `
-El captcha es un método de seguridad de la plataforma, debes de ingresar las letras/números de la imágen superior, por último da click en "Enviar"
+El captcha es un método de seguridad de la plataforma, debes de ingresar las letras/números de la imágen superior "RP6VTL", por último da click en "Enviar"
 `;
 
-var HTML_text = [SAT_inicio0, SAT_inicio1, SAT_inicio2, SAT_inicio3, SAT_inicio4,Login1]
-var tutorial_text = [boton_factura_electronica, boton_servicio_facturacion, boton_servicios_facturacion, boton_facturacion_cuentas, boton_ejecutar_linea,login,
-                    RFC_login, contrasena_login, captcha_login]
+// Posición 9
+var datos_incorrectos = `
+Los datos ingresados no son correctos, intentalo nuevamente
+`;
 
+// Posición 10
+var engrane_opciones =`
+Ahora haz click en el engrane de configuración de la parte superior derecha
+`;
+
+// Posición 11
+var opcion_datos_emisor = `
+Muy bien, ahora comenzaras realizando las configuraciones necesarias para la facturación, haz click en "Configuración de datos del emisor"
+`;
+
+// Posición 12
+var registro_emisor = `
+Llegaste hasta la configuración de datos del emisor, lo más importante de este apartado es el Registro Emisor
+`;
+
+// Posición 13
+var nombre_razon_social = `
+En este apartado debes de agregar tu nombre, denominación o razón social inscrito a tu RFC
+`;
+
+// Posición 14
+var nombre_comercial = `
+Si así lo deseas y cuentas con un nombre comercial puedes agregarlo aquí y también el logotipo, pero para este tutorial no será necesario. Ya con los datos anteriores ingresados recuerda hacer click en guardar
+`;
+
+var HTML_text = [SAT_inicio0, SAT_inicio1, SAT_inicio2, SAT_inicio3, SAT_inicio4, Login1, config_pt1, config_pt2, config_datos_emisor]
+
+var tutorial_text = [boton_factura_electronica, boton_servicio_facturacion, boton_servicios_facturacion, boton_facturacion_cuentas, boton_ejecutar_linea,login,
+                    RFC_login, contrasena_login, captcha_login, datos_incorrectos,
+                    engrane_opciones, opcion_datos_emisor, registro_emisor, nombre_razon_social, nombre_comercial]
+
+
+// Actualizar textos de tutorial
 async function actualizarTexto(pos){
     document.getElementById("tutorialText").innerHTML = tutorial_text[pos];
 }
 
+// Regresar al paso anterior
 async function anterior()
 {
     contador = contador - 2
     siguiente();
 }
 
+// Continuar al paso siguiente
 async function siguiente(){
     contador = contador + 1
     console.log(contador)
     document.getElementById("tuto-window").innerHTML = HTML_text[contador];
+}
+
+// Validación de inicio de sesión
+async function val_login(){
+    RFC = document.getElementById("RFC").value
+    contrasena = document.getElementById("contrasena").value
+    captcha = document.getElementById("captcha").value
+
+    if (RFC == "ABCD123456XXX" && contrasena == "sat-sim" && captcha == "RP6VTL")
+    {
+        siguiente()
+        actualizarTexto(10)
+    }
+
+    else
+    {
+        actualizarTexto(9)
+    }
+}
+
+// Funciones para el desarrollo
+async function mover()
+{
+    posicion = document.getElementById("paso").value
+    contador = parseInt(posicion)
+    document.getElementById("tuto-window").innerHTML = HTML_text[posicion];
+}
+
+async function mostrar()
+{
+    posicion = document.getElementById("paso").value
+    document.getElementById("tutorialText").innerHTML = tutorial_text[posicion];
 }
